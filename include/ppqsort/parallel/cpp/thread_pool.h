54d5acc28c0370cc06115ae0d799726fb26d68e1
GabTux <hevrgabr@fit.cvut.cz>
2024-03-09 14:26:18 +0100

    Simplify threadpool, use atomic::wait

--- include/ppqsort/parallel/cpp/thread_pool.h
+++ include/ppqsort/parallel/cpp/thread_pool.h
@@ -35,51 +35,55 @@ namespace ppqsort::impl::cpp {
                 // check if there are any tasks in queue or being executed, if not we can peacefully stop
                 // otherwise, wait for signal
                 // after the last thread is finished, it will signal that all tasks are finished
-                if (handling_tasks_.load(std::memory_order_acquire) > 0)
+                if (total_tasks_.load(std::memory_order_acquire) > 0)
                         threads_done_semaphore_.acquire();
 
-                // send exit signal to all threads and wait for them
+                // request stops
                 for (std::size_t i = 0; i < threads_.size(); ++i) {
                     threads_[i].request_stop();
-                    threads_queues_[i].task_semaphore.release();
                 }
+
+                // wake all potentionally sleeping threads
+                pending_tasks_.store(1, std::memory_order_release);
+                pending_tasks_.notify_all();
+
                 for (auto& thread : threads_)
                     thread.join();
                 stopped = true;
             }
 
             void push_task(taskType&& task) {
+                // firstly signal, that we will have some tasks
+                total_tasks_.fetch_add(1, std::memory_order_release);
+
                 // relaxed is ok, because we do not care about the exact value
                 const std::size_t i = index_.fetch_add(1, std::memory_order_relaxed);
 
                 // try to push without blocking to any queue
-                // if no queue is available, try again for K times, before waiting for one
+                // if no queue is available, try again for K times, before waiting for lock
                 constexpr unsigned int K = 2;
                 for (std::size_t n = 0; n < threads_count_ * K; ++n) {
-                    if (threads_queues_[(i + n) % threads_count_].queue_.try_push(std::forward<taskType>(task))) {
-                        after_push(i, n);
+                    if (threads_queues_[(i + n) % threads_count_].try_push(std::forward<taskType>(task))) {
+                        pending_tasks_.fetch_add(1, std::memory_order_release);
+                        pending_tasks_.notify_one();
                         return;
                     }
                 }
 
-                // all queues busy, wait for the first one to be free
-                threads_queues_[i % threads_count_].queue_.push(std::forward<taskType>(task));
-                after_push(i, 0);
+                // all queues busy, wait for the first one
+                threads_queues_[i % threads_count_].push(std::forward<taskType>(task));
+                pending_tasks_.fetch_add(1, std::memory_order_release);
+                pending_tasks_.notify_one();
             }
 
         private:
-            void after_push(const std::size_t i, const std::size_t n) {
-                pending_tasks_.fetch_add(1, std::memory_order_release);
-                handling_tasks_.fetch_add(1, std::memory_order_release);
-                threads_queues_[(i + n) % threads_count_].task_semaphore.release();
-            }
 
             bool get_next_task(const unsigned int id) {
                 bool found = false;
 
                 // spin around all queues to find a task, start with my queue
                 for (std::size_t n = 0; n < threads_count_; ++n) {
-                    if (auto task = threads_queues_[(id + n) % threads_count_].queue_.try_pop()) {
+                    if (auto task = threads_queues_[(id + n) % threads_count_].try_pop()) {
                         run_task(std::forward<taskType>(task.value()));
                         found = true;
                         break;
@@ -88,7 +92,7 @@ namespace ppqsort::impl::cpp {
 
                 // if all queues busy or empty, check our queue if empty
                 if (!found) {
-                    if (auto task = threads_queues_[id].queue_.pop()) {
+                    if (auto task = threads_queues_[id].pop()) {
                         run_task(std::forward<taskType>(task.value()));
                         found = true;
                     }
@@ -100,43 +104,31 @@ namespace ppqsort::impl::cpp {
             void run_task(taskType&& task) {
                 pending_tasks_.fetch_sub(1, std::memory_order_release);
                 task();
-                handling_tasks_.fetch_sub(1, std::memory_order_release);
+                total_tasks_.fetch_sub(1, std::memory_order_release);
             }
 
             void worker(const std::stop_token& stop_token, const unsigned int id) {
                 while (true) {
-                    // sleep until signalled about new tasks
-                    threads_queues_[id].task_semaphore.acquire();
-
-                    // while there are tasks, execute them (mine or stolen)
-                    while (pending_tasks_.load(std::memory_order_acquire) > 0) {
-                        if (!get_next_task(id))
-                            break;
-                    }
-
-                    // check if we were last working thread and eventually signal about all tasks finished
-                    if (handling_tasks_.load(std::memory_order_acquire) == 0)
-                        threads_done_semaphore_.release();
-
+                    // sleep until there are any tasks in queues
+                    pending_tasks_.wait(0,std::memory_order_acquire);
                     if (stop_token.stop_requested())
                         break;
 
-                    // I have no tasks, so prioritize me
-                    index_.store(id, std::memory_order_relaxed);
+                    // while there are tasks, execute them (mine or stolen)
+                    while (get_next_task(id));
+
+                    // no tasks are in queues or handled --> signal that our work is done
+                    if (total_tasks_.load(std::memory_order_acquire) == 0)
+                        threads_done_semaphore_.release();
                 }
             }
 
-            struct ThreadQueue {
-                TaskStack<taskType> queue_;
-                std::binary_semaphore task_semaphore{0};
-            };
-
             std::vector<std::jthread> threads_;
-            std::vector<ThreadQueue> threads_queues_;
+            std::vector<TaskStack<>> threads_queues_;
             const unsigned int threads_count_;
             alignas(parameters::cacheline_size) std::atomic<std::size_t> index_{0};
             alignas(parameters::cacheline_size) std::atomic<std::size_t> pending_tasks_{0};
-            alignas(parameters::cacheline_size) std::atomic<std::size_t> handling_tasks_{0};
+            alignas(parameters::cacheline_size) std::atomic<std::size_t> total_tasks_{0};
             std::binary_semaphore threads_done_semaphore_{0};   // used to wait for all tasks to finish
             std::mutex mtx_priority_;
             bool stopped = false;
